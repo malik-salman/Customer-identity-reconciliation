fbf8b03832603420633067ab46f92ef706c8c7dc
malik-salman <asmalik470478@gmail.com>
2026-02-28 04:24:48 +0530

    Updated server to use dynamic PORT for deployment

--- src/index.ts
+++ src/index.ts
@@ -164,6 +164,9 @@ app.post("/identify", async (req: Request, res: Response) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000 🚀");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
+  
